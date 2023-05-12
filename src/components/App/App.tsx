86680c611ea577564fa2c06295cb2d741b537fb3
davidpinkevich <davidpinkevich95@gmail.com>
2023-05-12 23:23:37 +0300

    feat: add base routing

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,25 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Main from '../../pages/Main';
+import Favorites from '../../pages/Favorites';
+import Vacancy from '../../pages/Vacancy';
+import Header from '../Header/Header';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Header />}>
+            <Route index element={<Main />} />
+            <Route path="favorites" element={<Favorites />} />
+            <Route path="vacancies" element={<Main />} />
+            <Route path="/vacancies/:id" element={<Vacancy />} />
+          </Route>
+          <Route path="*" element={<div>Not found</div>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
