1cc506c81c40996a028a0bfe1d987235f923d0e7
davidpinkevich <davidpinkevich95@gmail.com>
2023-05-13 20:09:15 +0300

    feat: add consts for url

---
+++ src/constants/index.ts
@@ -0,0 +1,14 @@
+import { TAuthenticationData } from '../types';
+
+const AUTH_DATA: TAuthenticationData = {
+  CLIENT_SECRET:
+    'v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948',
+  LOGIN: 'sergei.stralenia@gmail.com',
+  PASSWORD: 'paralect123',
+  CLIENT_ID: '2356',
+  X_SECRET_KEY: 'GEU4nvd3rej*jeh.eqp',
+};
+
+const BASE_URL = 'https://startup-summer-2023-proxy.onrender.com/2.0/';
+
+export { AUTH_DATA, BASE_URL };
