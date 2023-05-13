72420062298f65f66c41032c56ff8d76d22c4313
davidpinkevich <davidpinkevich95@gmail.com>
2023-05-13 20:08:22 +0300

    feat: create loader component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,13 @@
+import './Loading.scss';
+
+function Loading() {
+  return (
+    <div className="loading">
+      Ищем вакансии <span className="loading-first"></span>
+      <span className="loading-second"></span>
+      <span className="loading-third"></span>
+    </div>
+  );
+}
+
+export default Loading;
