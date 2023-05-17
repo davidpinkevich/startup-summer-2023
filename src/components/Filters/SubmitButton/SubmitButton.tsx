8ce9d7c4cd9cad55480030cb2d12abe9e906713f
davidpinkevich <davidpinkevich95@gmail.com>
2023-05-17 23:54:27 +0300

    feat: create submit button

---
+++ src/components/Filters/SubmitButton/SubmitButton.tsx
@@ -0,0 +1,21 @@
+import { useSelector } from 'react-redux';
+import { Button } from '@mantine/core';
+import { TStore } from '../../../types';
+
+function SubmitButton() {
+  const { loadingData } = useSelector((state: TStore) => state.jobs);
+  return (
+    <Button
+      loading={loadingData === 'loading'}
+      type="submit"
+      radius="8px"
+      size="lg"
+      loaderPosition="center"
+      styles={{ root: { fontSize: 14, fontWeight: 500, fontFamily: 'Inter' } }}
+    >
+      {loadingData === 'start' && 'Применить'}
+    </Button>
+  );
+}
+
+export default SubmitButton;
