786f6e8af63116aef0e7a382e0c5302f7bba261c
davidpinkevich <davidpinkevich95@gmail.com>
2023-05-17 20:49:53 +0300

    refactor: change name from salary

---
+++ src/components/Filters/FromSalary/FromSalary.tsx
@@ -0,0 +1,34 @@
+import { NumberInput } from '@mantine/core';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeFrom } from '../../../redux/slices/SliceFilters';
+import { TStore } from '../../../types';
+import { styles } from './styles';
+
+function FromSalary() {
+  const { fromSalary } = useSelector((state: TStore) => state.filters);
+  const { loadingData } = useSelector((state: TStore) => state.jobs);
+
+  const dispatch = useDispatch();
+
+  function changeInput(value: number | '') {
+    dispatch(changeFrom(value));
+  }
+  console.log('fromSalary: ', fromSalary);
+
+  return (
+    <NumberInput
+      disabled={loadingData === 'loading'}
+      min={0}
+      value={fromSalary}
+      onChange={changeInput}
+      radius="8px"
+      rightSectionWidth={40}
+      type="number"
+      placeholder="От"
+      description="Оклад"
+      styles={styles}
+    />
+  );
+}
+
+export default FromSalary;
