4565e998f611c7066a7344184c2ee028e89d50a8
davidpinkevich <davidpinkevich95@gmail.com>
2023-05-20 18:50:17 +0300

    feat: add disabled logic for clear button

--- src/components/Filters/ClearButton/ClearButton.tsx
+++ src/components/Filters/ClearButton/ClearButton.tsx
@@ -1,15 +1,18 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Button } from '@mantine/core';
 import cross from '../../../assets/icons/cross.svg';
 import { clearFilters } from '../../../redux/slices/SliceFilters';
+import { TStore } from '../../../types';
 import { styles } from './styles';
 
 function ClearButton() {
+  const { loadingData } = useSelector((state: TStore) => state.jobs);
   const dispatch = useDispatch();
 
   return (
     <Button
       onClick={() => dispatch(clearFilters())}
+      disabled={loadingData === 'loading'}
       h="20px"
       styles={styles}
       variant="subtle"
