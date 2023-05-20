2fc61472a68fd8e67453722157cc6d2b4429fa8c
davidpinkevich <davidpinkevich95@gmail.com>
2023-05-20 18:33:45 +0300

    feat: add logic local storage favorites

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,35 +1,79 @@
-import { useSelector } from 'react-redux';
-import { TStore } from '../../types';
+import { useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import marker from '../../assets/icons/marker.svg';
+import starActive from '../../assets/icons/star-active.svg';
+import starEmpty from '../../assets/icons/star-empty.svg';
+import { TDataFindVacancies, TStore } from '../../types';
+import { setFavorites } from '../../redux/slices/sliceJobs';
 import './Item.scss';
+import { NavLink } from 'react-router-dom';
 
 type TItem = {
-  id: number;
+  data: TDataFindVacancies;
 };
 
-function Item({ id }: TItem) {
-  console.log('id: ', id);
+function Item({ data }: TItem) {
+  const local = useLocation();
   const { vacancies } = useSelector((state: TStore) => state.jobs);
-  const from = vacancies[id].payment_from ? `от ${vacancies[id].payment_from}` : '';
-  const to = vacancies[id].payment_to ? `до ${vacancies[id].payment_to}` : '';
-  const salary = `з/п ${from} ${to} ${vacancies[id].currency || ''}`;
-  // console.log('vacancies: ', vacancies);
-  return (
-    <div className="jobs__item">
-      <p className="jobs__item-profession">{vacancies[id].profession}</p>
-      <div className="jobs__item-employment employment">
-        <p className="employment__salary">{salary}</p>
-        <span>•</span>
-        <p className="employment__type">{vacancies[id].type_of_work.title}</p>
-      </div>
+  const favorites: Array<TDataFindVacancies> = JSON.parse(localStorage.getItem('favorites') || '');
+  const [active, setActive] = useState(false);
+  const dispatch = useDispatch();
 
-      <div className="jobs__item-town">
-        <span>
-          <img src={marker} alt="marker" />
-        </span>
-        <p className="jobs__item-town-title">{vacancies[id].town.title}</p>
+  useEffect(() => {
+    if (favorites.filter((item) => item.id === data.id).length) setActive(true);
+  }, [data.id, favorites]);
+
+  const from = data.payment_from ? `от ${data.payment_from}` : '';
+  const to = data.payment_to ? `до ${data.payment_to}` : '';
+  const salary = `з/п ${from} ${to} ${data.currency || ''}`;
+
+  function addLs(event: React.MouseEvent<HTMLButtonElement>, numb: number) {
+    event.preventDefault();
+    const ls: Array<TDataFindVacancies> = JSON.parse(localStorage.getItem('favorites') || '');
+    const index = ls.findIndex((item) => item.id === numb);
+    if (index === -1) {
+      const check = vacancies.filter((item) => item.id === numb);
+      localStorage.setItem('favorites', JSON.stringify([...ls, ...check]));
+      if (local.pathname === '/favorites') {
+        dispatch(setFavorites([...ls, ...check]));
+      }
+      setActive(true);
+    } else if (index >= 0) {
+      ls.splice(index, 1);
+      localStorage.setItem('favorites', JSON.stringify(ls));
+      if (local.pathname === '/favorites') {
+        dispatch(setFavorites(ls));
+      }
+      setActive(false);
+    }
+  }
+
+  return (
+    <NavLink to={`/vacancies/${data.id}`}>
+      <div className="jobs__item">
+        <button
+          onClick={(event) => {
+            addLs(event, data.id);
+          }}
+          className="jobs__item-button"
+        >
+          <img src={active ? starActive : starEmpty} />
+        </button>
+        <p className="jobs__item-profession">{data.profession}</p>
+        <div className="jobs__item-employment employment">
+          <p className="employment__salary">{salary}</p>
+          <span>•</span>
+          <p className="employment__type">{data.type_of_work.title}</p>
+        </div>
+        <div className="jobs__item-town">
+          <span>
+            <img src={marker} alt="marker" />
+          </span>
+          <p className="jobs__item-town-title">{data.town.title}</p>
+        </div>
       </div>
-    </div>
+    </NavLink>
   );
 }
 
