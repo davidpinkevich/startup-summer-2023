75813112a63324dedbba2795a0c24b176ea74280
davidpinkevich <davidpinkevich95@gmail.com>
2023-05-21 18:01:07 +0300

    feat: create vacancy page

---
+++ src/pages/Vacancy/Vacancy.tsx
@@ -0,0 +1,36 @@
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getData, clearDescription } from '../../redux/slices/sliceJobs';
+import { BASE_URL } from '../../constants';
+import { TAppDispatch } from '../../redux/store/store';
+import Item from '../../components/Item/Item';
+import Loading from '../../components/Loading/Loading';
+import { TStore } from '../../types';
+import './Vacancy.scss';
+
+function Vacancy() {
+  const param = useParams();
+  const { description, loadingData } = useSelector((state: TStore) => state.jobs);
+  const dispatch = useDispatch<TAppDispatch>();
+  const url = `${BASE_URL}vacancies/${param.id}`;
+  useEffect(() => {
+    dispatch(clearDescription());
+    dispatch(getData(url));
+  }, [dispatch, url]);
+
+  return (
+    <main className="vacancy">
+      {loadingData === 'loading' && <Loading type={false} />}
+      {!!description.length && <Item data={description[0]} />}
+      {!!description.length && (
+        <div
+          className="vacancy__description"
+          dangerouslySetInnerHTML={{ __html: description[0].vacancyRichText }}
+        />
+      )}
+    </main>
+  );
+}
+
+export default Vacancy;
